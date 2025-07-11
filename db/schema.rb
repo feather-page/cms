61721255ec43015a299ecc5a269a07f00123cc5a
Robert Curth <robert.curth@techgenossen.de>
2025-07-11 09:38:34 +0200

    Creating a book.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_14_132721) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_11_062121) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
@@ -41,6 +41,28 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_14_132721) do
     t.uuid "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "authors_books", id: false, force: :cascade do |t|
+    t.uuid "book_id", null: false
+    t.uuid "author_id", null: false
+    t.index ["author_id", "book_id"], name: "index_authors_books_on_author_id_and_book_id"
+    t.index ["book_id", "author_id"], name: "index_authors_books_on_book_id_and_author_id"
+  end
+
+  create_table "books", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "public_id"
+    t.uuid "site_id", null: false
+    t.uuid "post_id"
+    t.string "title"
+    t.string "emoji"
+    t.string "author"
+    t.datetime "read_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_books_on_post_id"
+    t.index ["public_id"], name: "index_books_on_public_id", unique: true
+    t.index ["site_id"], name: "index_books_on_site_id"
   end
 
   create_table "deployment_targets", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -303,6 +325,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_14_132721) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "books", "posts"
+  add_foreign_key "books", "sites"
   add_foreign_key "deployment_targets", "sites"
   add_foreign_key "images", "sites"
   add_foreign_key "navigation_items", "navigations"
---
+++ spec/system/book_features_spec.rb
@@ -0,0 +1,36 @@
+describe 'Book features' do
+  describe 'creating a new book' do
+    it 'creates a new bok' do
+      as_user do |user|
+        site = create(:site, users: [user])
+
+        visit new_site_book_path(site)
+
+        fill_in 'Title', with: 'My Book'
+        fill_in 'Author', with: 'Foo Bar'
+        fill_in 'Emoji', with: '📕'
+
+        fill_in 'Read at', with: '2020-01-01'
+
+        expect do
+          click_on 'Create Book'
+        end.to change { site.books.count }.by(1)
+
+        created_book = site.books.last
+        expect(created_book.title).to eq('My Book')
+        expect(created_book.author).to eq('Foo Bar')
+        expect(created_book.read_at.to_date).to eq(Date.new(2020, 1, 1))
+      end
+    end
+  end
+
+  describe 'editing a book' do
+    it 'edits a book' do
+      as_user do |user|
+        site = create(:site, users: [user])
+        book = create(:book, site:)
+
+        visit site_books_path(site)
+    end
+  end
+end
